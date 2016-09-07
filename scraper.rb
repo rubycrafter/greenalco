d7c051226f34e59a47cc3bd29374a385130d400e
Ikonnikov Anton <rubycrafter92@gmail.com>
2016-09-07 19:15:41 +0500

    added basic scraper of main page

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,66 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'open-uri'
+require 'nokogiri'
+require 'scraperwiki'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+class Parser
+  attr_accessor :url, :tag, :doc, :headers, :catalog
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+  def initialize
+    @tag = Hash.new
+    @url = 'http://greenalco.ru'
+    @tag[:main_category] = '.box-category ul li a'
+    @tag[:group_item] = '.box-category ul li a'
+    @doc = Nokogiri::HTML(open(url))
+    @headers = %w(name paramsynonym category currency price discount description briefdescription)
+    @catalog = []
+    configure_scraper
+  end
+
+  def call
+    add_header
+    groups = scan_main_page
+    groups.each do |group|
+      scan_group(group)
+    end
+  end
+
+  private
+
+  def add_record(arr)
+    @catalog << arr
+    save_to_sqlite(arr)
+  end
+
+  def add_header
+    add_record(@headers)
+  end
+
+  def scan_main_page
+    scan_menu
+  end
+
+  def scan_menu
+    groups = Hash.new
+    @doc.css(@tag[:main_category]).each do |row|
+      groups[row.content] = row['href']
+    end
+
+    groups
+  end
+
+  def scan_group(group)
+  end
+
+  def scan_item(item)
+  end
+
+  def configure_scraper
+    ScraperWiki.config = { db: 'data.sqlite', default_table_name: 'data' }
+  end
+
+  def save_to_sqlite(arr)
+    ScraperWiki.save_sqlite(arr, Hash[@headers.zip arr])
+  end
+end
+
+Parser.new.call
