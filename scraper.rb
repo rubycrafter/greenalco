e0859f7faa74044abd81eef0de0ce83787a0ba25
Ikonnikov Anton <rubycrafter92@gmail.com>
2016-09-08 18:50:29 +0500

    done

--- scraper.rb
+++ scraper.rb
@@ -1,23 +1,32 @@
 require 'open-uri'
 require 'nokogiri'
 require 'scraperwiki'
+require 'mechanize'
 
 class Parser
   attr_accessor :url, :tag, :doc, :headers, :catalog
 
   def initialize
     @tag = Hash.new
-    @url = 'http://greenalco.ru'
     @tag[:main_category] = '.box-category ul li a'
-    @tag[:group_item] = '.box-category ul li a'
-    @doc = Nokogiri::HTML(open(url))
-    @headers = %w(name paramsynonym category currency price discount description briefdescription)
+    @tag[:item] = '.product-grid .name a'
+
+    @tag[:name] = 'h1'
+    @tag[:category] = '.breadcrumb a' #second one is a categori
+    @tag[:currency] = '.price span' #needs regexp
+    @tag[:currency2] = '.price' #needs regexp
+    @tag[:price] = '.price span' #needs regexp
+    @tag[:price2] = '.price' #needs regexp
+    @tag[:description] = '.tab-content'
+
+    @url = 'http://greenalco.ru'
+    @agent = Mechanize.new
+    @headers = %w(name paramsynonym category currency price description url)
     @catalog = []
     configure_scraper
   end
 
   def call
-    add_header
     groups = scan_main_page
     groups.each do |group|
       scan_group(group)
@@ -40,8 +49,10 @@ class Parser
   end
 
   def scan_menu
+    p 'Scanning main menu'
+    @page = @agent.get(@url)
     groups = Hash.new
-    @doc.css(@tag[:main_category]).each do |row|
+    @page.search(@tag[:main_category]).each do |row|
       groups[row.content] = row['href']
     end
 
@@ -49,9 +60,35 @@ class Parser
   end
 
   def scan_group(group)
+    p "Scanning group #{group[0]}"
+    @agent.transact do
+      @agent.click(group[0])
+      @agent.page.search(@tag[:item]).each do |item|
+        scan_item item
+      end
+    end
   end
 
   def scan_item(item)
+    arr = []
+    @agent.transact do
+      @agent.click(item.content)
+      arr << @agent.page.at(@tag[:name]).content
+      arr << item['href'][/.*\/(.*)\.html/, 1]
+      arr << @agent.page.search(@tag[:category])[1].content
+
+      unless @agent.page.at(@tag[:price]).nil?
+        arr << @agent.page.at(@tag[:currency]).content.split(' ')[1]
+        arr << @agent.page.at(@tag[:price]).content.split(' ')[0]
+      else
+        arr << @agent.page.at(@tag[:currency2]).content.split(' ')[1]
+        arr << @agent.page.at(@tag[:price2]).content.split(' ')[0]
+      end
+
+      arr << @agent.page.at(@tag[:description]).content.strip
+      arr << item['href']
+    end
+    save_to_sqlite arr
   end
 
   def configure_scraper
@@ -59,7 +96,8 @@ class Parser
   end
 
   def save_to_sqlite(arr)
-    ScraperWiki.save_sqlite(arr, Hash[@headers.zip arr])
+    p "Saving #{arr[0]}"
+    ScraperWiki.save_sqlite(["url"], Hash[@headers.zip arr])
   end
 end
 
